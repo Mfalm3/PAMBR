bdaca426d36bf51ece2522609e4b42aeaccbbde0
Joe Waithaka <waitha28ka@gmail.com>
2019-06-13 18:44:21 +0300

    Add config class

---
+++ src/Config/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mfalm3\Config;
+
+
+class Config
+{
+    const PATH_TO_DB = __DIR__.'/../../db/';
+
+    const PATH_TO_APP_LOG = __DIR__.'/../../res/logs';
+
+    const PATH_TO_VIEWS = __DIR__.'/../../res/views';
+
+    const PATH_TO_VIEWS_CACHE = __DIR__.'/../../res/cache';
+
+    const PATH_TO_UPLOAD = __DIR__.'/../../assets/';
+
+}
