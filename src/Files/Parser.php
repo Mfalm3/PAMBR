72abef6917e3c57da6a9f593466a01f08db7dee3
Joe Waithaka <waitha28ka@gmail.com>
2019-06-13 18:43:19 +0300

    Refactor the parser operations

--- src/Files/Parser.php
+++ src/Files/Parser.php
@@ -3,32 +3,79 @@
 namespace Mfalm3\Files;
 
 use XMLReader;
-use DOMDocument;
+use Mfalm3\Debug\Logger;
 
 class Parser
 {
     public $reader;
+    public $msgs;
+    public $sms;
+
 
     public function __construct()
     {
         $this->reader = new XMLReader();
+        $this->msgs = array();
+
     }
 
-    public function read($file)
+
+    public function open($file)
     {
-          $xml = simplexml_load_file($file);
-        $dom = new DOMDocument;
-        $dom->loadXml($xml);
+        try
+        {
+            Logger::log('Preparing to open file');
+            $this->reader->open($file);
+            Logger::log('File Opened');
 
-//        $this->reader->open($file);
-        $myfile = fopen("testfile.txt", "w");
-            while($xml) {
-                for($i=0;$i <= $dom->getElementsByTagName('sms')->length;$i++){
-                fwrite($myfile, $xml->sms[$i]['address']."\n");
-            }
-            }
-            fclose($myfile);
+        }
 
+        catch (\Exception $e)
+        {
+            Logger::log('error: '. $e->getMessage());
+        }
+    }
+
+
+    public function read()
+    {
+
+        while($this->reader->read())
+        {
+            $this->sms = new \stdClass();
+
+            if ($this->reader->nodeType === XMLREADER::ELEMENT && $this->reader->localName === 'sms')
+            {
+
+                $this->sms->protocol = $this->reader->getAttribute('protocol');
+                $this->sms->address = $this->reader->getAttribute('address');
+                $this->sms->date = $this->reader->getAttribute('date');
+                $this->sms->type = $this->reader->getAttribute('type');
+                $this->sms->subject = $this->reader->getAttribute('subject');
+                $this->sms->body = $this->reader->getAttribute('body');
+                $this->sms->toa = $this->reader->getAttribute('toa');
+                $this->sms->sc_toa = $this->reader->getAttribute('sc_toa');
+                $this->sms->service_center = $this->reader->getAttribute('service_center');
+                $this->sms->read = $this->reader->getAttribute('read');
+                $this->sms->status = $this->reader->getAttribute('status');
+                $this->sms->locked = $this->reader->getAttribute('locked');
+                $this->sms->date_sent = $this->reader->getAttribute('date_sent');
+                $this->sms->readable_date = $this->reader->getAttribute('readable_date');
+                $this->sms->contact_name = $this->reader->getAttribute('contact_name');
+
+                array_push($this->msgs, $this->sms);
+
+            }
+
+        }
+
+        return $this->msgs;
+    }
+
+
+    public function __destruct()
+    {
         $this->reader->close();
+        Logger::log('Closed File');
     }
 }
